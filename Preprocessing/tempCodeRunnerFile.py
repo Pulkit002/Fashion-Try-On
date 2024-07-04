ea35a5c41611a9df779f45c048131cf69faa2063
Pulkit <guptapulkit175@gmail.com>
2024-07-04 13:24:25 +0530

    Added preprocessing files

---
+++ Preprocessing/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+python Self-Correction-Human-Parsing-master\\simple_extractor.py --dataset 'atr' --gpu 'None' --model-restore 'python Self-Correction-Human-Parsing-master\\checkpoints\\exp-schp-201908301523-atr.pth' --input-dir 'C:\\Users\\Pulkit\\Desktop\\Preprocessing\\data\\image' --output-dir 'C:\\Users\\Pulkit\\Desktop\\Preprocessing\\data\\image-parse-v3'
