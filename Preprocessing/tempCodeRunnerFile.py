eb13e1e9072fb679d43b890d0c279d6d7cf9d8e8
aakarsh12bansal <120028847+aakarsh12bansal@users.noreply.github.com>
2024-08-06 16:10:24 +0530

    Removed extra preprocessing

--- Preprocessing/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-python Self-Correction-Human-Parsing-master\\simple_extractor.py --dataset 'atr' --gpu 'None' --model-restore 'python Self-Correction-Human-Parsing-master\\checkpoints\\exp-schp-201908301523-atr.pth' --input-dir 'C:\\Users\\Pulkit\\Desktop\\Preprocessing\\data\\image' --output-dir 'C:\\Users\\Pulkit\\Desktop\\Preprocessing\\data\\image-parse-v3'
